70e52bd7d519749227ed3583c1879e454f705978
Mo-Aziz <mohamed.aref32@gmail.com>
2023-08-06 11:44:41 -0500

    added grafbase to the project using grafbase sdk

---
+++ components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+import { NavLinks } from "@/constants";
+import AuthProvider from "./AuthProvider";
+
+const Navbar = () => {
+  const session = null;
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart ga-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={116} height={43} alt="logo" />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/profile">Share your work</Link>{" "}
+          </>
+        ) : (
+          <AuthProvider />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
